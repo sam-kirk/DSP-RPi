34a3382086579dbae3d8f8aa8d3211b75dc113c6
Sam <kirkysam9@gmail.com>
2021-03-25 15:49:21 +0000

    Updated for Pi Use

--- for_pi/image_capture_timed.py
+++ for_pi/image_capture_timed.py
@@ -4,33 +4,33 @@ from time import sleep
 from datetime import datetime
 
 if __name__ == '__main__':
-    cont = True
+    camera = PiCamera()
+    
+    cont = True    
     print('-- TIMED IMAGE CAPTURE --')
 
     # continue until user exits
     while cont:
-        t = input('How many seconds delay? (minimum 5s)')
+        t = float(input('How long is your flight time in minutes?\n'))
+        d = float(input('How many seconds delay? (minimum 3s)\n'))
         # 5s minimum to allow time for capture and save
-        if t < 5:
-            t = 5
+        if d < 3:
+            d = 3
+        
+        # how many pictures can be taken in that time with that delay
+        x = int(t * 60//d)
+        print(x)
+        print('Every ' + str(d) + ' seconds a picture will be taken for ' + str(t) + ' minutes.')
+        print('This will result in ' + str(x) + ' pictures')
 
-        m = input('How long is your flight time in minutes?')
-        # how many picture can be taken in that time with that delay
-        x = m // t * 60
-        print('Every' + t + 'seconds a picture will be taken for ' + m + ' minutes.')
-        print('This will result in ' + x + 'pictures')
-
-        start = ''
-        while start != 'Y' or 'N':
-            start = input('Enter y to begin or n to cancel.').upper()
-
-        if start == 'Y':
-            # start of image capture loop
-            for i in range(x):
-                sleep(t)
-                timestamp = datetime.now().strftime("_%Y-%m-%d_%H-%M-%S")
-                image_src = "static/raw-image" + timestamp + ".png"
-                camera.capture('image_src')
+        # start of image capture loop
+        for i in range(x):
+            print('image ' + str(i+1) + ' start')
+            sleep(d)
+            timestamp = datetime.now().strftime("_%Y-%m-%d_%H-%M-%S")
+            image_src = "raw_images/" + timestamp + ".png"
+            camera.capture(image_src)
+            print('image ' + str(i) + ' done')
 
         # continue or exit
         resp = input('Exit? (y/n)').upper()
