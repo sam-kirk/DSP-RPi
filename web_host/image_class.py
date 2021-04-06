a0c21ea76d58918441c4f092b9dc618415293c59
Sam <kirkysam9@gmail.com>
2021-04-06 15:07:06 +0100

    Compares 2 images

--- web_host/image_class.py
+++ web_host/image_class.py
@@ -326,3 +326,50 @@ class Image:
                 cv2.imshow("ROI", roi)
                 cv2.waitKey(0)
                 cv2.destroyAllWindows()'''
+
+    def similarity_features(self, second_img_path):
+        # Based on code by Alexander Mordvintsev & Abid K. 2013 found at:
+        # https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_feature2d/py_feature_homography/py_feature_homography.html
+
+        MIN_MATCH_COUNT = 10
+        FLANN_INDEX_KDTREE = 0
+
+        img1 = cv2.imread(self.filepath + self.name.split(".")[0] + "_ndvi-c.png")
+        img2 = cv2.imread(second_img_path)
+
+        img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+        # sift
+        sift = cv2.SIFT_create()
+
+        kp1, des1 = sift.detectAndCompute(img1, None)
+        kp2, des2 = sift.detectAndCompute(img2, None)
+
+        # feature matching
+        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+        search_params = dict(checks=50)
+
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        matches = flann.knnMatch(des1, des2, k=2)
+
+        # store all the good matches as per Lowe's ratio test.
+        good = []
+        for m, n in matches:
+            if m.distance < 0.7 * n.distance:
+                good.append(m)
+
+        if len(good) > MIN_MATCH_COUNT:
+            match = True
+        else:
+            match = False
+
+        img3 = cv2.drawMatches(img1, kp1, img2, kp2, good, None, flags=2)
+        print(match)
+
+        cv2.imshow('Matches', img3)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+        return match
