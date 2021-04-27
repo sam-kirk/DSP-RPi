c376b9b371e40f0284b4ac189abc6f0683901bef
Sam <kirkysam9@gmail.com>
2021-04-27 14:42:26 +0100

    Tidy of capture scripts

--- for_pi/image_capture_host_trigger.py
+++ for_pi/image_capture_host_trigger.py
@@ -1,27 +1,26 @@
 # Runs a flask web app that allows pictures to be taken on trigger click
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template
 from picamera import PiCamera
 from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route('/image-capture', methods=['POST', 'GET'])
+
+@app.route('/image_capture', methods=['POST', 'GET'])
 def image_capture():
     try:
         take_picture = request.form.get("take_picture")
         print('Take picture? = ', take_picture)
         if take_picture == "t":
-            timestamp = datetime.now().strftime("_%Y-%m-%d_%H-%M-%S")
+            timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
             image_src = "static/raw-image" + timestamp + ".png"
-            # print('bing')
             camera.start_preview()
             # sleep(2)
             camera.capture(image_src)
             camera.stop_preview()
-            # print('bong')
-            return render_template('image-capture.html')
+            return render_template('image_capture.html')
     finally:
-        return render_template('image-capture.html')
+        return render_template('image_capture.html')
 
 
 if __name__ == '__main__':
